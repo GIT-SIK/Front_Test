a89cded20f69056b6b47b72a5b3287dd37ad0e3f
GIT-SIK <xeno_s@naver.com>
2024-01-23 01:36:31 +0900

    modal, grid, pagination

--- page_etc/modal/user/info/info.js
+++ page_etc/modal/user/info/info.js
@@ -1,9 +1,13 @@
+/* 데이터 테스트 */
+import { gridData } from './dummy.js';
+
+/* 실행 영역 */
 $(document).ready(function ($) {
 
+    /* 모달 Open Close 영역 */
     $("#infoModalOpenBtn").on("click", function (event) {
         viewInfoModal(true);
     });
-
 
     $("body").on("click", function (event) {
         if (event.target.className == 'backon') {
@@ -11,17 +15,58 @@ $(document).ready(function ($) {
         }
     });
 
-
     $(".close-btn img").on("click", function (event) {
         viewInfoModal(false);
-
     });
+
+
+    /* Grid 영역 */
+    var grid = new tui.Grid({
+        el: document.getElementById('infoGrid'),
+        data: gridData,
+        scrollX: false,
+        scrollY: true,
+        columns: [
+            {
+                title: 'Id',
+                name: 'id'
+            },
+            {
+                title: 'Name',
+                name: 'name'
+            }
+        ],
+        rowHeaders: ['rowNum'],
+        pageOptions: {
+            useClient: true,
+            perPage: 5
+        }
+    });
+
+
+    /* 페이지 테스트 영역 */
+    // const appendBtn = document.getElementById('appendBtn');
+    // const prependBtn = document.getElementById('prependBtn');
+
+    // const appendedData = {
+    //     id: 359999,
+    //     name: 'INPUT',
+    // };
+
+    // appendBtn.addEventListener('click', () => {
+    //     grid.appendRow(appendedData);
+    // });
+
+    // prependBtn.addEventListener('click', () => {
+    //     grid.prependRow(appendedData);
+    // });
+
 
 });
 
 
 
-/* Signup 모달 */
+/* 모달 뷰 */
 function viewInfoModal(bool) {
     if (bool == true) {
         $(".info-modal").show();
